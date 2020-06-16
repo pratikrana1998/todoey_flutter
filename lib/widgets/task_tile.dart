5c1869115e66c7a9626438d1518550f244fb7483
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-06-16 18:53:27 +0530

    postioning bottomSheet, creating ListView biulder using callbacks

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
+      title: Text(
+          taskTitle,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+
+      ),
       trailing: Checkbox(
-        value: false,
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
       ),
     );
   }
 }
+
